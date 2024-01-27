8b365790e627c2653f10dd8c8bfff66f7d78ca9b
piperlaney123 <piperddehoyos@gmail.com>
2024-01-27 12:08:34 -0700

    test

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
 print("Hello World!")
 print("updating file now....")
 print("modifying for branch")
+
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+strings.append("hello")
+strings.append("world")
+
+second_name = names[1]
+
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
+
