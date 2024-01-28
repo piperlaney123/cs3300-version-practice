59f5116ced33b9b563501fea69e9dc392d026f19
piperlaney123 <piperddehoyos@gmail.com>
2024-01-27 19:12:55 -0700

    more exercise code

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ print(numbers)
 print(strings)
 print("The second name on the names list is %s" % second_name) """
 
-x = object()
+""" x = object()
 y = object()
 
 # TODO: change this code
@@ -35,4 +35,19 @@ print("big_list contains %d objects" % len(big_list))
 if x_list.count(x) == 10 and y_list.count(y) == 10:
     print("Almost there...")
 if big_list.count(x) == 10 and big_list.count(y) == 10:
-    print("Great!")
+    print("Great!") """
+
+# Modify this function to return a list of strings as defined above
+def list_benefits():
+    return ["More organized code", "More readable code", "Easier code reuse", "Allowing programmers to share and connect code together"]
+
+# Modify this function to concatenate to each benefit - " is a benefit of functions!"
+def build_sentence(benefit):
+    return benefit + " is a benefit of functions!"
+
+def name_the_benefits_of_functions():
+    list_of_benefits = list_benefits()
+    for benefit in list_of_benefits:
+        print(build_sentence(benefit))
+
+name_the_benefits_of_functions()
