0f29ff59e13dcb6a91c5edde0fbd8a435203c3e6
piperlaney123 <piperddehoyos@gmail.com>
2024-01-27 19:20:22 -0700

    moar exercise code

--- test.py
+++ test.py
@@ -37,7 +37,7 @@ if x_list.count(x) == 10 and y_list.count(y) == 10:
 if big_list.count(x) == 10 and big_list.count(y) == 10:
     print("Great!") """
 
-# Modify this function to return a list of strings as defined above
+""" # Modify this function to return a list of strings as defined above
 def list_benefits():
     return ["More organized code", "More readable code", "Easier code reuse", "Allowing programmers to share and connect code together"]
 
@@ -50,4 +50,49 @@ def name_the_benefits_of_functions():
     for benefit in list_of_benefits:
         print(build_sentence(benefit))
 
-name_the_benefits_of_functions()
+name_the_benefits_of_functions() """
+
+""" # define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+
+# your code goes here
+car1 = Vehicle()
+car2 = Vehicle()
+
+car1.color = "red"
+car1.name = "Fer"
+car1.kind = "convertible"
+car1.value = 60000
+
+car2.color = "blue"
+car2.name = "Jump"
+car2.kind = "van"
+car2.value = 10000
+
+
+# test code
+print(car1.description())
+print(car2.description()) """
+
+phonebook = {  
+    "John" : 938477566,
+    "Jack" : 938377264,
+    "Jill" : 947662781
+}  
+# your code goes here
+phonebook["Jake"] = 938273443
+phonebook.pop("Jill")
+
+# testing code
+if "Jake" in phonebook:  
+    print("Jake is listed in the phonebook.")
+    
+if "Jill" not in phonebook:      
+    print("Jill is not listed in the phonebook.") 
